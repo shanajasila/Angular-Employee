c10fe8e53a11c1af686fd73704e744fa6166070c
shanajasila <shanajasilam@gmail.com>
2022-11-24 16:20:44 +0530

    delete

---
+++ src/app/delete-employ/delete-employ.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-employ',
+  templateUrl: './delete-employ.component.html',
+  styleUrls: ['./delete-employ.component.css']
+})
+export class DeleteEmployComponent {
+
+}
