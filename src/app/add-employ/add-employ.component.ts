2ac62d3a2dc1b39f61c6f78f9fba067c4b89d60a
shanajasila <shanajasilam@gmail.com>
2022-11-24 16:08:55 +0530

    added

---
+++ src/app/add-employ/add-employ.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-employ',
+  templateUrl: './add-employ.component.html',
+  styleUrls: ['./add-employ.component.css']
+})
+export class AddEmployComponent {
+
+}
