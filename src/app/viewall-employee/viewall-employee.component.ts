0fc1cedb2ce424914fb14827a9798df6f2b3314b
shanajasila <shanajasilam@gmail.com>
2022-11-25 11:11:46 +0530

    binding

--- src/app/viewall-employee/viewall-employee.component.ts
+++ src/app/viewall-employee/viewall-employee.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./viewall-employee.component.css']
 })
 export class ViewallEmployeeComponent {
-
+data:any=[
+  {"name":"shana",
+"designation":"Trainee",
+"salary":12546777,
+"phonenumber":435634456,
+"companyname":"ast"
+},
+{"name":"asha",
+"designation":"Trainee",
+"salary":23457,
+"phonenumber":6756332,
+"companyname":"anest"
+},
+{"name":"james",
+"designation":"Trainee",
+"salary":987637,
+"phonenumber":435634456,
+"companyname":"ibs"
+},
+{"name":"alice",
+"designation":"Developer",
+"salary":35467,
+"phonenumber":8999282666,
+"companyname":"df"
+},
+{"name":"sam",
+"designation":"Trainee",
+"salary":657899,
+"phonenumber":235617901,
+"companyname":"ast"
+}
+]
 }
